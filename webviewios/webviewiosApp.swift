1d2821587a554053e42820ff240e346cb06312d1
Pushpendra Khandelwal <pushpendra.khandelwal@coindcx.com>
2023-04-20 15:35:23 +0530

    Initial Commit

---
+++ webviewios/webviewiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  webviewiosApp.swift
+//  webviewios
+//
+//  Created by Pushpendra Khandelwal on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct webviewiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
